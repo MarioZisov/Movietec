d5ed567b9fef5f597b8884c35c0520c8e1fbf320
zisov4eto <zisov4eto@gmail.com>
2017-03-20 21:29:33 +0200

    Configure automapper

---
+++ MovietecWebApp/Movietec.App/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Movietec.Models.DbModels;
+using Movietec.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movietec.App.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            this.CreateMap<Customer, CustomerDto>();
+            this.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
