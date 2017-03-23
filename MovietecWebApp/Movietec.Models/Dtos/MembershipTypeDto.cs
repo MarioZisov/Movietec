79e8d9542dc6a520e2a9a3fda4fc3d5394ad68fb
zisov4eto <zisov4eto@gmail.com>
2017-03-23 22:39:13 +0200

    Add DataTable library.

---
+++ MovietecWebApp/Movietec.Models/Dtos/MembershipTypeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movietec.Models.Dtos
+{
+    public class MembershipTypeDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
